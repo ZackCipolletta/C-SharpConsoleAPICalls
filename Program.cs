f346a027685d419db8cd01cfb9fce41feb0ca853
Zachary <ZackCipolletta@gmail.com>
2023-03-21 20:01:16 -0700

    Add ability to parse JSON response from API response objects and handle multimedia arrays

--- Program.cs
+++ Program.cs
@@ -2,16 +2,32 @@
 using System.Threading.Tasks;
 using RestSharp;
 using ConsoleApiCall.Keys;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
 
 namespace ApiTest
 {
-  class Program 
+  class Program
   {
-    static void Main()
+    static void Main(string[] args)
     {
       Task<string> apiCallTask = ApiHelper.ApiCall(EnvironmentVariables.ApiKey);
       string result = apiCallTask.Result;
-      Console.WriteLine(result);
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      List<Article> articleList = JsonConvert.DeserializeObject<List<Article>>(jsonResponse["results"].ToString());
+
+      foreach (Article article in articleList)
+      {
+        Console.WriteLine($"Section: {article.Section}");
+        Console.WriteLine($"Title: {article.Title}");
+        Console.WriteLine($"Abstract: {article.Abstract}");
+        Console.WriteLine($"Url: {article.Url}");
+        Console.WriteLine($"Byline: {article.Byline}");
+        Console.WriteLine($"Item_Type: {article.Item_Type}");
+        Console.WriteLine("------------------------");
+      }
     }
   }
 
